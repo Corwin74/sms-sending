153dd711086a9f34047cc57ad647cd3f010795c0
Samokhin Alexander <icorwin74@gmail.com>
2023-05-22 15:43:49 +0300

    Add backend draft

--- server.py
+++ server.py
@@ -1,23 +1,75 @@
-from quart import render_template, websocket
+import os
+import warnings
+import json
+from contextvars import ContextVar
+from unittest.mock import patch
+from quart import request, websocket
 from quart_trio import QuartTrio
 import trio
+from trio import TrioDeprecationWarning
+import dotenv
+from smsc_api import request_smsc, request_smsc_side_effect
+
 
 app = QuartTrio(__name__)
+smsc_login = ContextVar('smsc_login')
+smsc_password = ContextVar('smsc_password')
+ssl_context = ContextVar('ssl_context')
 
 
-@app.route("/")
+@app.route('/')
 async def hello():
     async with await trio.open_file('index.html') as f:
         page = await f.read()
     return page
 
 
-@app.websocket("/ws")
+@app.websocket('/ws')
 async def ws():
     while True:
-        await websocket.send("hello")
-        await websocket.send_json({"hello": "world"})
+        await trio.sleep(5)
+        await websocket.send('''
+    {
+        "msgType": "SMSMailingStatus", "SMSMailings": [
+        {
+        "timestamp": 1123131392.734,
+        "SMSText": "Сегодня гроза! Будьте осторожны!",
+        "mailingId": "1",
+        "totalSMSAmount": 345,
+        "deliveredSMSAmount": 47,
+        "failedSMSAmount": 5
+        },
+        {
+        "timestamp": 1323141112.924422,
+        "SMSText": "Новогодняя акция!!! Приходи в магазин и получи скидку!!!",
+        "mailingId": "new-year",
+        "totalSMSAmount": 3993,
+        "deliveredSMSAmount": 801,
+        "failedSMSAmount": 0
+        }
+    ] 
+    }''')
 
 
-if __name__ == "__main__":
+
+@app.route('/send/', methods=['POST'])
+async def send():
+    form = await request.form
+    with patch('__main__.request_smsc') as mock_func:
+        mock_func.side_effect = request_smsc_side_effect
+        payload = {'msg': form['text']}
+        parced_resp = await request_smsc(
+            'GET',
+            'send',
+            payload=payload,
+        )
+    print(parced_resp)
+    return parced_resp
+
+
+if __name__ == '__main__':
+    dotenv.load_dotenv()
+    smsc_login.set(os.environ['SMSC_USER'])
+    smsc_password.set(os.environ['SMSC_API_PASSWORD'])
+    warnings.filterwarnings(action='ignore', category=TrioDeprecationWarning)
     app.run()
